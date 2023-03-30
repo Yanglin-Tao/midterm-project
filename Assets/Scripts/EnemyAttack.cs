dc48d9933105fb53690f3a6d007a95285a91eb3e
Yanglin Tao <yt2061@nyu.edu>
2023-03-30 11:06:07 -0400

    Updated death and attacks

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     private Animator Animator;
+    GameManager _gameManager;
 
     void Start(){
         Animator = GetComponent<Animator>();
+        _gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        Animator.SetBool("Attack", false);
     }
 
     void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.tag == "Player"){
-            Animator.SetBool("Attack", true);
+            if (_gameManager.getScore() < 1){
+                Animator.SetBool("Attack", true);
+            }
         }
     }
 }
