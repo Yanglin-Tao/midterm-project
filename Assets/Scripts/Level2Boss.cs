72b9fb9cfd5ba151dc402e23bbbadf87de6061fc
Mitchellzhou1 <mitche.zhou1@gmail.com>
2023-04-05 19:03:20 -0400

    making bully death

--- Assets/Scripts/Level2Boss.cs
+++ Assets/Scripts/Level2Boss.cs
@@ -11,14 +11,20 @@ public class Level2Boss : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private Collider2D _collider2D;
 
+    public AudioClip deathSound;
+    AudioSource _audioSource;
+
     int bulletSpeed = 600;
     public GameObject bulletPrefab;
     public Transform spawnPoint;
+    int health;
+    bool flag = true;
 
     void Start(){
         Animator = GetComponent<Animator>();
         _gameManager = GameObject.FindObjectOfType<GameManager>();
         _rigidbody = GetComponent<Rigidbody2D>();
+        health = 3;
 
     }
 
@@ -44,5 +50,25 @@ public class Level2Boss : MonoBehaviour
             Destroy(Bullet, 2);
             Animator.SetBool("canThrow", false);
         }
+
+        if (collision.gameObject.tag == "Burger"){
+            health -= 1;
+            if (health == 0){
+                StartCoroutine(Death(2));
+                if (flag){
+                    Instantiate(explosion, transform.position, Quaternion.identity);
+                    flag = false;
+                }
+                Animator.SetFloat("Dead", 1);
+            }
+        }
+
+    }
+
+    IEnumerator Death (int seconds) {
+        _audioSource.PlayOneShot(deathSound);
+        int counter = seconds;
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
     }
 }
