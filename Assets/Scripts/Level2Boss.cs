350853543c13a3513d853855659a0d7ca13b008b
Mitchellzhou1 <mitche.zhou1@gmail.com>
2023-04-05 18:46:58 -0400

    the bully can throw tomato

--- Assets/Scripts/Level2Boss.cs
+++ Assets/Scripts/Level2Boss.cs
@@ -7,7 +7,6 @@ public class Level2Boss : MonoBehaviour
 {
     private Animator Animator;
     GameManager _gameManager;
-    public int neededScore = 10;
     public float speed = 5;
     private Rigidbody2D _rigidbody;
     private Collider2D _collider2D;
@@ -32,15 +31,17 @@ public class Level2Boss : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.tag == "Pen"){
             speed = -speed;
+            bulletSpeed = -bulletSpeed;
             // get current localScale
             Vector3 localScale = transform.localScale;
             // flip x axis
             transform.localScale = new Vector3(-localScale.x, localScale.y, localScale.z);
             
             Animator.SetBool("canThrow", true);
+            print("this");
             GameObject Bullet = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
             Bullet.GetComponent<Rigidbody2D>().AddForce(new Vector3(bulletSpeed, 0, 1));
-            Destroy(Bullet, 3);
+            Destroy(Bullet, 2);
             Animator.SetBool("canThrow", false);
         }
     }
